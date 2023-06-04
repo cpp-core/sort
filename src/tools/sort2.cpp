41e92a96f19ab7f471eed3fffec5d23a42cb8f06
Mark Melton <mark.melton@notlem.com>
2023-06-04 12:19:34 -0400

    WIP/concurrency

--- src/tools/sort2.cpp
+++ src/tools/sort2.cpp
@@ -18,7 +18,7 @@ int tool_main(int argc, const char *argv[]) {
     std::mt19937_64 rng;
     std::generate(data.begin(), data.end(), [&]() { return d(rng); });
 
-    size_t nworkers = 10;
+    size_t nworkers = 2;
     std::vector<std::thread> workers;
     
     core::timer::Timer timer;
@@ -33,10 +33,10 @@ int tool_main(int argc, const char *argv[]) {
     std::sort(sample.begin(), sample.end());
 
     size_t bucketsize = data.size() / nworkers;
+    auto pivot = sample[sample.size() / 2];
     for (auto i = 0; i < nworkers; ++i) {
 	auto sdx = i * bucketsize;
 	auto edx = std::min((i + 1) * bucketsize, data.size());
-	auto pivot = sample[(i * sample.size()) / nworkers];
 	workers.emplace_back([&,sdx,edx,pivot]() {
 	    auto ldx = sdx - 1;
 	    auto rdx = edx;
@@ -51,6 +51,8 @@ int tool_main(int argc, const char *argv[]) {
 		    break;
 		std::swap(data[ldx], data[rdx]);
 	    }
+	    while (rdx < edx and data[rdx] < pivot)
+		++rdx;
 	    std::sort(&data[sdx], &data[rdx]);
 	    std::sort(&data[rdx], &data[edx]);
 	});
@@ -59,8 +61,25 @@ int tool_main(int argc, const char *argv[]) {
     for (auto& worker : workers)
 	worker.join();
 
+    std::vector<uint64_t> result(data.size());
+    size_t idx{}, ldx{}, rdx{bucketsize};
+    while (ldx < bucketsize and rdx < data.size()) {
+	auto cmp = data[ldx] <=> data[rdx];
+	if (cmp <= 0)
+	    result[idx++] = data[ldx++];
+	if (cmp >= 0)
+	    result[idx++] = data[rdx++];
+    }
+    std::copy(&data[ldx], &data[bucketsize], &result[idx]);
+    std::copy(&data[rdx], &data[data.size()], &result[idx]);
+    std::swap(data, result);
+
     timer.stop();
     cout << (1e-9 * timer.elapsed().count()) << endl;
+
+    for (auto i = 1; i < data.size(); ++i)
+	if (data[i-1] > data[i])
+	    cout << i << " " << data[i-1] << " " << data[i]  << endl;
     
     return 0;
 }
